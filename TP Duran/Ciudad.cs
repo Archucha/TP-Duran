e6aa13bde022c8692b540540e1246a8497bac78f
info-1 <info-1@INFO1-PC6>
2025-05-07 19:05:03 -0300

    Agregar archivos de proyecto.

---
+++ TP Duran/Ciudad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Duran
+{
+    internal class Ciudad
+    {
+        public string  Nombre { get; set; }
+        public int CantHabitantes { get; set; }
+        public int Superficie { get; set; }
+    }
+}
